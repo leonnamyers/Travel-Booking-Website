7a651dd9d64a4190cba80bdc609b3c0b1e705119
Jianliang <jianlan4200@gmail.com>
2024-09-01 17:47:37 +1000

    Update Catalogue.java

--- assignment1/src/main/java/com/iotbay/Model/Catalogue.java
+++ assignment1/src/main/java/com/iotbay/Model/Catalogue.java
@@ -1,6 +1,16 @@
 package com.iotbay.Model;
+import java.util.*;
+import java.sql.Date;
+import java.sql.Time;
+import java.io.Serializable;
 
-public class Catalogue {
-    
+public class Catalogue implements Serializable{
+    private double price;
+    private Time startTime;
+    private Date startDate;
+    private String name;
+    private String serviceType;
+    private int availability;
+    private String img;
 
 }
