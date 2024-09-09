6cea6ab5d613c4e65b3af7b1bce72a6908ee3e5e
AnnemareeH <160560543+AnnemareeH@users.noreply.github.com>
2024-09-10 01:40:50 +1000

    Staff User Management Controllers

---
+++ assignment1/src/main/java/com/iotbay/Controller/RegisterPanelController.java
@@ -0,0 +1,171 @@
+package com.iotbay.Controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.iotbay.Dao.DBManager;
+import com.iotbay.Model.Address;
+import com.iotbay.Model.Customer;
+import com.iotbay.Model.Staff;
+
+public class RegisterPanelController extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)   throws ServletException, IOException { 
+
+        try {
+            HttpSession session = request.getSession();
+            String registeredUserType = request.getParameter("userType");
+            String email = request.getParameter("email");
+            String password = request.getParameter("password"); 
+            String firstName = request.getParameter("first_name");
+            String lastName = request.getParameter("last_name");
+
+            DBManager manager = (DBManager) session.getAttribute("manager");
+ 
+            UserValidation.clear(session);
+
+            // initial pre-validations for all fields that apply to both Customer and Staff users
+            /* Error Handling
+            if (manager.isDuplicateEmail(email)) {
+                session.setAttribute("duplicateEmail", "Error: Email is already registered.");
+                forwardWithError(request, response, session);
+                return;
+            } else if (!UserValidation.isEmailValid(email)) {
+                session.setAttribute("emailError", "Error: Email incorrectly formatted. Please try again.");
+                forwardWithError(request, response, session);
+                return;
+            } else if (!UserValidation.isPasswordValid(password)) {
+                session.setAttribute("passwordError", "Error: Password incorrectly formatted. Please try again.");
+                forwardWithError(request, response, session);
+                return;
+            } else if (!UserValidation.isFieldAlphaNum(firstName)) {
+                session.setAttribute("firstNameError", "Error: First name incorrectly formatted. Please try again.");
+                forwardWithError(request, response, session);
+                return;
+            } else if (!UserValidation.isFieldAlphaNum(lastName)) {
+                session.setAttribute("lastNameError", "Error: Last name incorrectly formatted. Please try again.");
+                forwardWithError(request, response, session);
+                return;
+            }
+                */
+            
+            // then check the user type and perform more specific validations
+            // add the user to the relevant DB table and then update the user session
+            if (registeredUserType.equalsIgnoreCase("customer")) {
+                // Customer User Registration
+                Address address = new Address(request.getParameter("street_address"), Integer.parseInt(request.getParameter("postcode")), request.getParameter("city"), request.getParameter("state"));
+                String phoneNumber = request.getParameter("phone_number");
+                String mobileNumber = request.getParameter("mobile_number");
+
+                Customer customerUser = new Customer(email, password, firstName, lastName, address);
+
+                if (phoneNumber != null && !phoneNumber.isEmpty() && phoneNumber.matches("\\d+")) {
+                    ((Customer) customerUser).setHomePhoneNumber(Integer.parseInt(phoneNumber));
+                    if (!UserValidation.isPhoneNumberValid(phoneNumber)) {
+                        session.setAttribute("homePhoneError", "Error: Home Phone Number should be 8-16 digits. Please try again.");
+                        forwardWithError(request, response, session);
+                        return;
+                    }
+                }
+                if (mobileNumber != null && !mobileNumber.isEmpty() && mobileNumber.matches("\\d+")) {
+                    ((Customer) customerUser).setMobilePhoneNumber(Integer.parseInt(mobileNumber));
+                    if (!UserValidation.isPhoneNumberValid(mobileNumber)) {
+                        session.setAttribute("mobilePhoneError", "Error: Mobile Phone Number should be 8-16 digits. Please try again.");
+                        forwardWithError(request, response, session);
+                        return;
+                    }
+                }
+
+                if (!UserValidation.isFieldAlphaNum(address.getStreetAddress())) {
+                    session.setAttribute("streetAddressError", "Error: Street Address incorrectly formatted. Please try again.");
+                    forwardWithError(request, response, session);
+                    return;
+                } else if (!UserValidation.isPostcodeValid(address.getPostcode())) {
+                    session.setAttribute("postcodeError", "Error: Postcode should be 4 digits. Please try again.");
+                    forwardWithError(request, response, session);
+                    return;
+                } else if (!UserValidation.isFieldAlphaNum(address.getCity())) {
+                    session.setAttribute("cityError", "Error: City incorrectly formatted. Please try again.");
+                    forwardWithError(request, response, session);
+                    return;
+                }
+                
+                // doesn't replace session if current session is staff
+                if (!(session.getAttribute("user") instanceof Staff)) {
+                    session.setAttribute("user", customerUser); // Only set session user if it's self-registration
+                    response.sendRedirect("welcome.jsp");
+                } else {
+                    response.sendRedirect("userListPanel.jsp");
+                }
+
+            } else {
+                // Staff User Registration
+
+                String staffID = request.getParameter("staff_id");
+                String staffType = request.getParameter("staff_type");
+
+                Staff staff = new Staff(email, password, firstName, lastName);
+
+                if (staffID != null && !staffID.isEmpty() && staffID.matches("\\d+")) {
+                    staff.setStaffID(Integer.parseInt(staffID));
+                    if (!UserValidation.isStaffIdInvalid(staffID)) {
+                        session.setAttribute("staffIDError", "Error: Staff ID expects 8 digits. Please try again.");
+                        forwardWithError(request, response, session);
+                        return;
+                    }
+
+                    if (manager.isDuplicateStaffID(staffID)) {
+                        session.setAttribute("duplicateStaffID", "Error: Staff ID already in system.");
+                        forwardWithError(request, response, session);
+                        return;
+                    }
+                }
+
+                if (staffID != null && !staffType.isEmpty() && staffType.matches("\\d+")) {
+                    staff.setStaffTypeID(Integer.parseInt(staffType));
+                } else {
+                    staff.setStaffTypeID(1);
+                }
+
+                //manager.addStaff(staff, session.getId());
+                response.sendRedirect("userListPanel.jsp");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RegisterPanelController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void forwardWithError(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
+        String[] errorAttributes = {
+            "duplicateEmail",
+            "duplicateStaffID",
+            "emailError",
+            "passwordError",
+            "firstNameError",
+            "lastNameError",
+            "homePhoneError",
+            "mobilePhoneError",
+            "streetAddressError",
+            "postcodeError",
+            "cityError",
+            "staffIDError"
+        };
+
+        for (String attribute : errorAttributes) {
+            String errorMessage = (String) session.getAttribute(attribute);
+            if (errorMessage != null && !errorMessage.isEmpty()) {
+                request.setAttribute("errorMessage", errorMessage);
+                session.removeAttribute(attribute);
+                break;
+            }
+        }
+        request.getRequestDispatcher("register.jsp").forward(request, response);
+    }
+}
