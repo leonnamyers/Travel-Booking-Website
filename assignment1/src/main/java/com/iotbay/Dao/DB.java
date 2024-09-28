0d308c866bf05eeee5cf143ebb76e726e351439c
monica8823 <Bochi.Shu@student.uts.edu.au>
2024-09-28 13:55:25 +1000

    Add MySQL service in pipeline

--- assignment1/src/main/java/com/iotbay/Dao/DB.java
+++ assignment1/src/main/java/com/iotbay/Dao/DB.java
@@ -3,7 +3,9 @@ package com.iotbay.Dao;
 import java.sql.Connection;
 
 public abstract class DB {
-	protected String URL = "jdbc:mysql://localhost:3306/";
+	// protected String URL = "jdbc:mysql://localhost:3306/";
+	protected String URL = "jdbc:mysql://mysql:3306/";  // 使用 mysql 作为主机名
+
 	protected String db = "TravelWeb";
 	protected String dbuser = "root";
 	protected String dbpass =  "1234!@#$";
