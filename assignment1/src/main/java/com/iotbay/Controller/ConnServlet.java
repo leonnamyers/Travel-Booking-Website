929768602831ae6a1a9e35087b4582bbda449119
channy-g <160987384+channy-g@users.noreply.github.com>
2024-10-04 12:21:29 +1000

    test

--- assignment1/src/main/java/com/iotbay/Controller/ConnServlet.java
+++ assignment1/src/main/java/com/iotbay/Controller/ConnServlet.java
@@ -11,7 +11,15 @@ import javax.servlet.http.HttpSession;
 
 import com.iotbay.Dao.DBConnector;
 import com.iotbay.Dao.DBManager;
+<<<<<<< Updated upstream
 import com.iotbay.Model.Cart;
+=======
+import com.iotbay.Dao.FlightDAO;
+import com.iotbay.Dao.HotelDAO;
+import com.iotbay.Model.Cart;
+import com.iotbay.Model.Flight;
+import com.iotbay.Model.Hotel;
+>>>>>>> Stashed changes
 
 // A get request is sent to this Servlet using jsp:include for any JSP page that may potentially use the DB.
 // The DB connection and DBManager is only created once and stored in the session to be shared across the app
@@ -52,6 +60,15 @@ public class ConnServlet extends HttpServlet {
         
         try {
             manager = new DBManager(conn);
+<<<<<<< Updated upstream
+=======
+            flightDAOManager = new FlightDAO();
+            hotelDAOManager = new HotelDAO(conn);
+            ArrayList<Flight> flightList = flightDAOManager.fetchAllFlights();
+            ArrayList<Hotel> hotelList = hotelDAOManager.fetchAllHotels();
+            session.setAttribute("flightList", flightList);
+            session.setAttribute("hotelList", hotelList);
+>>>>>>> Stashed changes
         } catch (SQLException ex) {
             Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
---
+++ assignment1/src/main/java/com/iotbay/Model/DummyItems.java
@@ -0,0 +1,11 @@
+package com.iotbay.Model;
+
+public class DummyItems {
+
+    public static Flight createDummyFlight() {
+        return new Flight(
+
+        );
+    }
+}
+
