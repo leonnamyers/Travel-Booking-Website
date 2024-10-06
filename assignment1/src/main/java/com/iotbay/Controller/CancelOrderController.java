8afb5fcc88ea41ec6650d8448036328b859348ba
channy-g <160987384+channy-g@users.noreply.github.com>
2024-10-06 17:43:49 +1100

    update cancelorder controller

--- assignment1/src/main/java/com/iotbay/Controller/CancelOrderController.java
+++ assignment1/src/main/java/com/iotbay/Controller/CancelOrderController.java
@@ -3,15 +3,12 @@ package com.iotbay.Controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/CancelOrderController")
 public class CancelOrderController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
